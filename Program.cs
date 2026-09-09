86ab622d7a3fbceadab1fe33deda4beea79d1210
bucketSonyEricssonT700 <netumel881900@gmail.com>
2026-09-09 13:41:55 +1200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ using System.Text.RegularExpressions;
 		{
 			Console.WriteLine("1 - Ввести запрос\n2 - Посмотреть запросы\n3 - Удалить запрос");
 			string pick = Console.ReadLine();
-			string request = "NoNe";
+			string request = "";
 
 			switch(pick)
 			{
